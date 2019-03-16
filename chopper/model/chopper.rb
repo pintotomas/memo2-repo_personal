08f36965996984bfa9d49f30085bea9383b130b2
Tomas Eduardo Pinto <tomasp834@gmail.com>
2019-03-16 04:13:19 -0300

    Use of unless, fix other minor offences

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -1,9 +1,7 @@
 class Chopper
   def chop(position, array) # rubocop:disable Lint/UnusedMethodArgument
-    if array.size > 0
-        return 0
-    else
-        return -1
-    end	
-    end
+    return 0 unless array.empty?
+
+    -1
+  end
 end
