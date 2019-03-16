30de44acb62f1033187f58a4f104790fa17487cb
Tomas Eduardo Pinto <tomasp834@gmail.com>
2019-03-16 03:57:26 -0300

    fix offences

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -1,9 +1,9 @@
 class Chopper
   def chop(position, array) # rubocop:disable Lint/UnusedMethodArgument
-    if (array.size > 0)
-    	return 0
+    if array.size > 0
+        return 0
     else
-    	return -1
+        return -1
     end	
-  end
+    end
 end
