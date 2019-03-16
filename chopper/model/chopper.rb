cea562b647d8dba931209e79d45d170f484ccdac
Tomas Eduardo Pinto <tomasp834@gmail.com>
2019-03-16 17:12:29 -0300

    Chopper.sum with [1,3] working

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -9,6 +9,14 @@ class Chopper
   def sum(num_array)
     return 'vacio' if num_array.empty?
 
-    'uno'
+    sum = num_array.inject(:+)
+    NumToString.convert(sum)
+  end
+end
+
+class NumToString
+  def self.convert(number)
+    text_representation_of_numbers = { 1 => 'uno', 4 => 'cuatro' }
+    text_representation_of_numbers[number]
   end
 end
