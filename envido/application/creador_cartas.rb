53cf0aa1e23aa72f4cc8eabda1c3f3ab30aaf4d4
Tomas Eduardo Pinto <tomasp834@gmail.com>
2019-03-23 18:14:08 -0300

    Creador de cartas

--- envido/application/creador_cartas.rb
+++ envido/application/creador_cartas.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/ruby
 require_relative '../model/carta.rb'
 class Factory
-  def self.create_cards(_cards_represented_in_string)
-    []
+  def self.create_cards(cards_represented_in_string)
+    palos = { 'p' => 'basto', 'c' => 'copa',
+              'o' => 'oro', 'e' => 'espada' }
+
+    cards = cards_represented_in_string.split(',')
+    cards_to_play = []
+    cards.each do |card|
+      palo = palos[card[-1]]
+      valor = card[0...-1].to_i
+      nueva_carta = Carta.new(palo, valor)
+      cards_to_play.push(nueva_carta)
+    end
+    cards_to_play
   end
 end
