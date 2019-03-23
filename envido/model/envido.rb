7a898f2a08a8e1a15123ff0f0796dfc4a6f8c1cb
Tomas Eduardo Pinto <tomasp834@gmail.com>
2019-03-23 19:08:05 -0300

    First unit test and acceptance test 10 passing

--- envido/model/envido.rb
+++ envido/model/envido.rb
@@ -6,14 +6,7 @@ class Envido
   end
 
   def initialize(card_array)
-    valid_palos = %w[basto copa oro espada]
-    valid_valores = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]
-    card_array.each do |card|
-      raise PaloError unless valid_palos.include? card.valid_palo
-      return ValorError unless valid_valores.include? card.valor
-
-      @cards_on_game = card_array
-    end
+    @cards_on_game = card_array
   end
 
   attr_reader :cards_on_game
