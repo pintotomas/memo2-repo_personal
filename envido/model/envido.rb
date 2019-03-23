40c98a72146192f595c1142ec1f68c50f1c6e742
Tomas Eduardo Pinto <tomasp834@gmail.com>
2019-03-23 18:42:45 -0300

    Envido.initialize with exceptions

--- envido/model/envido.rb
+++ envido/model/envido.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/ruby
-
+require_relative 'exceptions.rb'
 class Envido
   def calculate
     -1
   end
 
   def initialize(card_array)
-    @cards_on_game = card_array
+    valid_palos = %w[basto copa oro espada]
+    valid_valores = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]
+    card_array.each do |card|
+      raise PaloError unless valid_palos.include? card.valid_palo
+      return ValorError unless valid_valores.include? card.valor
+
+      @cards_on_game = card_array
+    end
   end
 
   attr_reader :cards_on_game
---
+++ envido/model/exceptions.rb
@@ -0,0 +1,5 @@
+class PaloError < StandardError
+end
+
+class ValorError < StandardError
+end
