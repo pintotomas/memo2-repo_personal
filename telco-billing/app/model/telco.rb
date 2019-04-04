32b7363d650bbc0a73dded4edfe81c5567fe1536
tomaspinto <tomasp834@gmail.com>
2019-04-04 04:09:43 -0300

    test #3 cost for a call of a minute on a sunday

--- telco-billing/app/model/telco.rb
+++ telco-billing/app/model/telco.rb
@@ -2,8 +2,9 @@ require 'time'
 class Telco
   def call_cost(inicio, _fin)
     # Returns call cost
-    start_hour = Time.parse(inicio).hour
-    return 3.2 if start_hour < 20
+    time_stamp = Time.parse(inicio)
+    return 2.1 if time_stamp.saturday? || time_stamp.sunday?
+    return 3.2 if time_stamp.hour < 20
 
     1.8
   end
--- telco-billing/spec/telco_spec.rb
+++ telco-billing/spec/telco_spec.rb
@@ -11,4 +11,7 @@ describe 'Telco' do
   it 'Local call with one minute duration between 20 and 8 hs on weekdays should cost 1.8' do
     expect(telco.call_cost('20190211;22:30', '20190211;22:31')).to eq 1.8
   end
+  it 'Local call with one minute duration on weekend should cost 2.1' do
+    expect(telco.call_cost('20190210;14:30', '20190210;14:31')).to eq 2.1
+  end
 end
