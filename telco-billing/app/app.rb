770095b56c6cd960a72e450089169e7f17c056db
tomaspinto <tomasp834@gmail.com>
2019-04-05 13:42:19 -0300

    Fix align error

--- telco-billing/app/app.rb
+++ telco-billing/app/app.rb
@@ -1,8 +1,7 @@
 require 'sinatra'
 require 'json'
-require_relative 'model/telco'
-require_relative 'adapters/input_adapter.rb'
-require_relative 'adapters/output_adapter.rb'
+require_relative 'model'
+require_relative 'adapters'
 telco = Telco.new
 input_adapter = JSONInputAdapter.new
 output_adapter = JSONOutputAdapter.new
@@ -12,7 +11,8 @@ post '/llamadas' do
   call_info = input_adapter.interpret_call(input)
   call_start_time = 0
   call_end_time = 1
-  call_cost = telco.call_cost(call_info[call_start_time], call_info[call_end_time])
+  call = Call.new(call_info[call_start_time], call_info[call_end_time])
+  call_cost = telco.call_cost(call)
   output = output_adapter.present_call_cost(call_cost)
   status 201
   output
--- telco-billing/app/model/telco.rb
+++ telco-billing/app/model/telco.rb
@@ -1,21 +1,19 @@
 require 'time'
 class Telco
-  def call_cost(call_start_time, call_end_time)
+  #  def call_cost(call_start_time, call_end_time)
+  def call_cost(call)
     # Returns call cost
+    # to do: delegate to CallCostCalculator
     weekday_night_cost = 1.8
     weekday_day_cost = 3.2
     night_start_time = 20
     weekends_cost = 2.1
     one_minute = 60
-    call_start_time_parsed = Time.parse(call_start_time)
-    call_end_time_parsed = Time.parse(call_end_time)
-    difference = call_end_time_parsed - call_start_time_parsed
+    difference = call.duration
     difference_in_minutes = difference / one_minute
-    return difference_in_minutes * weekends_cost if call_start_time_parsed.saturday? ||
-                                                    call_start_time_parsed.sunday?
-    return difference_in_minutes * weekday_day_cost if call_start_time_parsed.hour <
-                                                       night_start_time
+    return difference_in_minutes * weekends_cost if call.started_weekend
+    return difference_in_minutes * weekday_day_cost if call.ended_before(night_start_time)
 
     difference_in_minutes * weekday_night_cost
-  end
+ end
 end
