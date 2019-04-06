1299b72bb9931398d25d109d1f94dd2ddf35820c
tomaspinto <tomasp834@gmail.com>
2019-04-06 03:16:58 -0300

    MinuteDetector detects correctly 2 minutes of a call in different situations

--- telco-billing/spec/minute_detector_spec.rb
+++ telco-billing/spec/minute_detector_spec.rb
@@ -1,6 +1,5 @@
 require 'rspec'
 require 'time'
-require 'byebug'
 require_relative '../app/model/minute_detector.rb'
 describe 'MinuteDetector' do
   let(:minute_detector) { MinuteDetector.new }
@@ -27,5 +26,28 @@ describe 'MinuteDetector' do
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
     week_night_minutes = minute_count['week_night_minutes']
     expect(week_night_minutes).to eq 1
+  end
+  it 'detector detects correctly the quantity of minutes of a Call of 2 minu on weekend ' do
+    start = Time.parse('20190209;14:30')
+    finish = Time.parse('20190209;14:32')
+    minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
+    weekend_minutes = minute_count['weekend_minutes']
+    expect(weekend_minutes).to eq 2
+  end
+
+  it 'detector detects correctly the quantity of minutes of Call of 2 min (daylight) on weekday ' do
+    start = Time.parse('20190212;14:30')
+    finish = Time.parse('20190212;14:32')
+    minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
+    week_day_minutes = minute_count['week_day_minutes']
+    expect(week_day_minutes).to eq 2
+  end
+
+  it 'detector detects correctly the quantity of minutes of Call of 2 min (night) on weekday ' do
+    start = Time.parse('20190212;20:30')
+    finish = Time.parse('20190212;20:32')
+    minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
+    week_night_minutes = minute_count['week_night_minutes']
+    expect(week_night_minutes).to eq 2
   end
 end
