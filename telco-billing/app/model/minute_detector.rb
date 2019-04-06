3a1a22ff4753362c40c60551f8031b57650a3c28
tomaspinto <tomasp834@gmail.com>
2019-04-06 03:23:19 -0300

    Forgot to add changes to MinuteDetector, stop build failing

--- telco-billing/app/model/minute_detector.rb
+++ telco-billing/app/model/minute_detector.rb
@@ -1,20 +1,25 @@
+
 class MinuteDetector
-  def self.differentiate_call_minutes(call_start_time, call_end_time)
-    # Returns a dictionary which has the count of how many of the
-    # minutes of the call were made on weekend,
-    # weekday during daylight or during night(Night is considered from 20pm to 8am).
-    minutes_result = { 'weekend_minutes' => 0 }
-    actual_call_time_aux = call_start_time
-    minute_increment = 60
-    while actual_call_time_aux < call_end_time
-      actual_call_time_aux += minute_increment
+   @type_of_minute = {0 => 'week_night_minutes', 1 => 'week_night_minutes', 2 => 'week_night_minutes', 3 => 'week_night_minutes', 4 => 'week_night_minutes', 5 => 'week_night_minutes',
+     6 => 'week_night_minutes', 7 => 'week_night_minutes', 8 => 'week_day_minutes', 9 => 'week_day_minutes', 10 => 'week_day_minutes', 11 => 'week_day_minutes', 12 => 'week_day_minutes',
+     13 => 'week_day_minutes', 14 => 'week_day_minutes', 15 => 'week_day_minutes', 16 => 'week_day_minutes', 17 => 'week_day_minutes', 18 => 'week_day_minutes', 19 => 'week_day_minutes',
+     20 => 'week_night_minutes', 21 => 'week_night_minutes', 22 => 'week_night_minutes', 23 => 'week_night_minutes'}
 
-      if actual_call_time_aux.sunday? || actual_call_time_aux.saturday?
-        minutes_result['weekend_minutes'] =
-          minutes_result['weekend_minutes'] + 1
+     def self.differentiate_call_minutes(call_start_time, call_end_time)
+      #Returns a dictionary which has the count of how many of the minutes of the call were made on weekend, 
+      #weekday during daylight or during night(Night is considered from 20pm to 8am).
+      minutes_result = {'week_night_minutes'=>0, 'week_day_minutes'=>0, 'weekend_minutes'=>0}
+      actual_call_time_aux = call_start_time
+      minute_increment = 60
+      while actual_call_time_aux < call_end_time
+        actual_call_time_aux += minute_increment  
+
+        if actual_call_time_aux.sunday? || actual_call_time_aux.saturday?
+          minutes_result['weekend_minutes'] = minutes_result['weekend_minutes'] + 1
+        else 
+          minutes_result[@type_of_minute[actual_call_time_aux.hour]] = minutes_result[@type_of_minute[actual_call_time_aux.hour]] + 1
+        end
       end
-
-    end
-    minutes_result
-  end
+      minutes_result
+     end
 end
