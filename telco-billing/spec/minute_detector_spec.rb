0ee2d38f9c498b76c1ea86a474aaa871f75dda56
tomaspinto <tomasp834@gmail.com>
2019-04-06 03:32:14 -0300

    Fix rubocop warnings

--- telco-billing/spec/minute_detector_spec.rb
+++ telco-billing/spec/minute_detector_spec.rb
@@ -5,7 +5,7 @@ require 'byebug'
 describe 'MinuteDetector' do
   let(:minute_detector) { MinuteDetector.new }
 
-  it 'detector detects correctly the quantity of minutes of a Call of 1 minu on weekend ' do
+  it 'detects correctly the quantity of minutes of a Call of 1 minu on weekend ' do
     start = Time.parse('20190209;14:30')
     finish = Time.parse('20190209;14:31')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
@@ -13,7 +13,7 @@ describe 'MinuteDetector' do
     expect(weekend_minutes).to eq 1
   end
 
-  it 'detector detects correctly the quantity of minutes of Call of 1 min (daylight) on weekday ' do
+  it 'detects correctly the quantity of minutes of Call of 1 min (daylight) on weekday ' do
     start = Time.parse('20190212;14:30')
     finish = Time.parse('20190212;14:31')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
@@ -21,14 +21,14 @@ describe 'MinuteDetector' do
     expect(week_day_minutes).to eq 1
   end
 
-  it 'detector detects correctly the quantity of minutes of Call of 1 min (night) on weekday ' do
+  it 'detects correctly the quantity of minutes of Call of 1 min (night) on weekday ' do
     start = Time.parse('20190212;20:30')
     finish = Time.parse('20190212;20:31')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
     week_night_minutes = minute_count['week_night_minutes']
     expect(week_night_minutes).to eq 1
   end
-  it 'detector detects correctly the quantity of minutes of a Call of 2 minu on weekend ' do
+  it 'detects correctly the quantity of minutes of a Call of 2 minu on weekend ' do
     start = Time.parse('20190209;14:30')
     finish = Time.parse('20190209;14:32')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
@@ -36,7 +36,7 @@ describe 'MinuteDetector' do
     expect(weekend_minutes).to eq 2
   end
 
-  it 'detector detects correctly the quantity of minutes of Call of 2 min (daylight) on weekday ' do
+  it 'detects correctly the quantity of minutes of Call of 2 min (daylight) on weekday ' do
     start = Time.parse('20190212;14:30')
     finish = Time.parse('20190212;14:32')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
@@ -44,21 +44,18 @@ describe 'MinuteDetector' do
     expect(week_day_minutes).to eq 2
   end
 
-  it 'detector detects correctly the quantity of minutes of Call of 2 min (night) on weekday ' do
+  it 'detects correctly the quantity of minutes of Call of 2 min (night) on weekday' do
     start = Time.parse('20190212;20:30')
     finish = Time.parse('20190212;20:32')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
     week_night_minutes = minute_count['week_night_minutes']
     expect(week_night_minutes).to eq 2
   end
-    it 'detector detects correctly the quantity of minutes of Call of 2 min between a friday night and a saturday ' do
-    #byebug
+  it 'detects correctly quantity of minutes of 2 min call between friday and a saturday ' do
     start = Time.parse('20190208;23:59')
     finish = Time.parse('20190209;00:01')
     minute_count = MinuteDetector.differentiate_call_minutes(start, finish)
-    week_night_minutes = minute_count['week_night_minutes']
-    weekend_minutes = minute_count['weekend_minutes']
-    expect(week_night_minutes).to eq 1
-    expect(weekend_minutes).to eq 1
+    expect(minute_count['week_night_minutes']).to eq 1
+    expect(minute_count['weekend_minutes']).to eq 1
   end
 end
