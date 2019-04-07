230298de65db92e4a15e329aa28edad22b8bce30
tomaspinto <tomasp834@gmail.com>
2019-04-07 16:26:38 -0300

    Test#31 TimeAux doesnt count minutes if start_time = end_time

--- telco-billing/app/model/time_aux.rb
+++ telco-billing/app/model/time_aux.rb
@@ -1,14 +1,24 @@
 class TimeAux
   # Contains auxiliary methods for working with Time objects from library 'time'
   @minute_increment = 60
-  def self.count_minutes(_t1, _t2, _params, minute_counter)
+  def self.count_minutes(time_start, time_end, params, minute_counter)
     # minute_counter is a MinuteCOunter object
-    # t1 and t2 must be Time objects
-    # Counts the minutes between t1 and t2 that satisfy params
+    # time_start and time_end must be Time objects
+    # Counts the minutes between time_start and time_end that satisfy params
     # params must be a dictionary with the format {day => {since => a, until => a}}
     # day must be an integer that maps to the week day following the Time gem convention
     # a and b must be the hours that you want to consider from the day 'day'
     # increments minute_counter when a time meets the requirements in params
-    minute_counter.inc
+    aux_time = time_start
+    while aux_time < time_end
+      aux_time += @minute_increment
+      actual_day = aux_time.wday
+      actual_hour = aux_time.hour
+      next unless params.key? actual_day
+
+      start_hour_limit = params[actual_day]['since']
+      end_hour_limit = params[actual_day]['until']
+      minute_counter.inc if actual_hour.between?(start_hour_limit, end_hour_limit)
+    end
   end
 end
--- telco-billing/spec/time_aux_spec.rb
+++ telco-billing/spec/time_aux_spec.rb
@@ -10,4 +10,11 @@ describe 'TimeAux' do
     TimeAux.count_minutes(Time.parse('20190211;14:30'), Time.parse('20190211;14:31'), params, minute_counter)
     expect(minute_counter.cost).to eq 2.5
   end
+
+  it 'time_aux doesnt count minutes if the times are equal' do
+    minute_counter = MinuteCounter.new(2.5)
+    params = { 1 => { 'since' => 14, 'until' => 15 } }
+    TimeAux.count_minutes(Time.parse('20190211;14:30'), Time.parse('20190211;14:30'), params, minute_counter)
+    expect(minute_counter.cost).to eq 0
+  end
 end
