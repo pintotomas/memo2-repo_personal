606e5212fdd55df1c76ab79ab3cad4570c831059
tomaspinto <tomasp834@gmail.com>
2019-04-06 21:02:09 -0300

    Test#29 incrementing minutes increments the cost of the minute

--- telco-billing/spec/minute_counter_spec.rb
+++ telco-billing/spec/minute_counter_spec.rb
@@ -7,4 +7,9 @@ describe 'LocalCallWeekDayMinuteCounter' do
   it 'At start, cost is 0 because the quantity of minutes is 0' do
     expect(minuteCounter.cost).to eq 0
   end
+
+  it 'Within one minute, cost is 2.5 because the quantity of minutes is 1' do
+    minuteCounter.inc
+    expect(minuteCounter.cost).to eq 2.5
+  end
 end
