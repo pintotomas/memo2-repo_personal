06b6b5a575712718390bb9967914d46a3611ab20
tomaspinto <tomasp834@gmail.com>
2019-04-07 16:31:16 -0300

    Test#32 TimeAux doesnt count minutes if they ocur during a day that should not be considered

--- telco-billing/spec/time_aux_spec.rb
+++ telco-billing/spec/time_aux_spec.rb
@@ -11,10 +11,16 @@ describe 'TimeAux' do
     expect(minute_counter.cost).to eq 2.5
   end
 
-  it 'time_aux doesnt count minutes if the times are equal' do
+  it 'time_aux doesnt count minutes if the start time and end time are equal' do
     minute_counter = MinuteCounter.new(2.5)
     params = { 1 => { 'since' => 14, 'until' => 15 } }
     TimeAux.count_minutes(Time.parse('20190211;14:30'), Time.parse('20190211;14:30'), params, minute_counter)
+    expect(minute_counter.cost).to eq 0
+  end
+  it 'time_aux doesnt doesnt count minutes if the day is not on params' do
+    minute_counter = MinuteCounter.new(2.5)
+    params = { 2 => { 'since' => 14, 'until' => 15 } }
+    TimeAux.count_minutes(Time.parse('20190211;14:30'), Time.parse('20190211;14:32'), params, minute_counter)
     expect(minute_counter.cost).to eq 0
   end
 end
