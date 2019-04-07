cde85e94b3c057e56b7efef5a21325638ae9a6c1
tomaspinto <tomasp834@gmail.com>
2019-04-07 16:42:33 -0300

    Test#33 TimeAux doesnt count minutes outside of the time range

--- telco-billing/spec/time_aux_spec.rb
+++ telco-billing/spec/time_aux_spec.rb
@@ -17,10 +17,16 @@ describe 'TimeAux' do
     TimeAux.count_minutes(Time.parse('20190211;14:30'), Time.parse('20190211;14:30'), params, minute_counter)
     expect(minute_counter.cost).to eq 0
   end
-  it 'time_aux doesnt doesnt count minutes if the day is not on params' do
+  it 'time_aux doesnt doesnt count minutes if they ocurr during a day that should not be considered' do
     minute_counter = MinuteCounter.new(2.5)
     params = { 2 => { 'since' => 14, 'until' => 15 } }
     TimeAux.count_minutes(Time.parse('20190211;14:30'), Time.parse('20190211;14:32'), params, minute_counter)
+    expect(minute_counter.cost).to eq 0
+  end
+  it 'time_aux doesnt count minutes if they are out of the time range of that day' do
+    minute_counter = MinuteCounter.new(2.5)
+    params = { 0 => { 'since' => 18, 'until' => 20 } }
+    TimeAux.count_minutes(Time.parse('20190224;14:35'), Time.parse('20190224;14:40'), params, minute_counter)
     expect(minute_counter.cost).to eq 0
   end
 end
