0889b3addc3cd9560ea834009afc4e5ba50be9a2
tomaspinto <tomasp834@gmail.com>
2019-04-08 00:22:24 -0300

    Test#42 International calls to rest of america

--- telco-billing/spec/international_call_spec.rb
+++ telco-billing/spec/international_call_spec.rb
@@ -10,4 +10,12 @@ describe 'InternationalCall' do
     international_call = InternationalCall.new('20190211;14:30', '20190211;14:32', '52')
     expect(international_call.cost).to eq 20
   end
+  it 'international call call to rest of america of 1 minute' do
+    international_call = InternationalCall.new('20190211;14:30', '20190211;14:31', '55')
+    expect(international_call.cost).to eq 6
+  end
+  it 'international call call to rest of america of 2 minutes' do
+    international_call = InternationalCall.new('20190211;14:30', '20190211;14:32', '55')
+    expect(international_call.cost).to eq 12
+  end
 end
