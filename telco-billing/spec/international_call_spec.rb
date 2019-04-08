c3f8d4a5ecd8fc9aab23b3a3cac241d7cb2c6dc9
tomaspinto <tomasp834@gmail.com>
2019-04-08 00:18:51 -0300

    Test#41 International call of 2 minutes to NA

--- telco-billing/spec/international_call_spec.rb
+++ telco-billing/spec/international_call_spec.rb
@@ -6,4 +6,8 @@ describe 'InternationalCall' do
     international_call = InternationalCall.new('20190211;14:30', '20190211;14:31', '52')
     expect(international_call.cost).to eq 10
   end
+  it 'international call call to NA of 2 minutes' do
+    international_call = InternationalCall.new('20190211;14:30', '20190211;14:32', '52')
+    expect(international_call.cost).to eq 20
+  end
 end
