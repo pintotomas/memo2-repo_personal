38ee6e75a7420414e85d7121c1cdb6592cf566c9
tomaspinto <tomasp834@gmail.com>
2019-04-07 23:30:00 -0300

    Test#39 national call of 6 minutes

--- telco-billing/spec/national_call_spec.rb
+++ telco-billing/spec/national_call_spec.rb
@@ -3,11 +3,15 @@ require_relative '../app/model/call/national_call'
 
 describe 'NationalCall' do
   it 'National call of 1 minute cost' do
-    national_call = NationalCall.new('20190211;14:31', '20190211;14:32')
+    national_call = NationalCall.new('20190211;14:30', '20190211;14:31')
     expect(national_call.cost).to eq 20
   end
   it 'National call of 5 minutes' do
-    national_call = NationalCall.new('20190211;14:31', '20190211;14:36')
+    national_call = NationalCall.new('20190211;14:30', '20190211;14:35')
     expect(national_call.cost).to eq 20
+  end
+  it 'National call of 6 minutes' do
+    national_call = NationalCall.new('20190211;14:30', '20190211;14:36')
+    expect(national_call.cost).to eq 21.5
   end
 end
