387fc10be62600dd97274461c484bf63fc4140ba
tomaspinto <tomasp834@gmail.com>
2019-04-08 02:14:59 -0300

    Test#45 factory creates a national call

--- telco-billing/spec/call_factory_spec.rb
+++ telco-billing/spec/call_factory_spec.rb
@@ -2,8 +2,6 @@ require 'rspec'
 require 'byebug'
 require_relative '../app/model/call/call_factory'
 
-require_relative '../app/model/call/local_call'
-
 describe 'CallFactory' do
   let(:callFactory) { CallFactory.new }
 
@@ -12,5 +10,12 @@ describe 'CallFactory' do
                   'fechahora_inicio': '20190211;14:30', 'fechahora_fin': '20190211;14:31' }
     local_call = callFactory.new_call(call_info)
     expect(local_call.class).to eq LocalCall
+  end
+
+  it 'Create a national call' do
+    call_info = { 'numero_origen': '54 011 4444 1111', 'numero_destino': '54 314 5555 6666',
+                  'fechahora_inicio': '20190211;14:30', 'fechahora_fin': '20190211;14:31' }
+    national_call = callFactory.new_call(call_info)
+    expect(national_call.class).to eq NationalCall
   end
 end
