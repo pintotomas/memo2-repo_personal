15e93d329df62b47c1b040504d778121c907826b
tomaspinto <tomasp834@gmail.com>
2019-04-08 02:07:29 -0300

    Test#44 CallFactory creates correctly a local call

---
+++ telco-billing/app/model/call/call_factory.rb
@@ -0,0 +1,27 @@
+require_relative '../phone'
+require_relative 'international_call'
+require_relative 'local_call'
+require_relative 'national_call'
+class CallFactory
+  def new_call(call_info)
+    # call_info is a dictionary with the following keys:
+    # 'numero_origen', 'numero_destino', 'fechahora_inicio', 'fechahora_fin'
+    # returns a call depending on these parameters.
+    # could be local call, national call or international call.
+
+    call_classes = 
+    #If area codes are equal, we have a local or national call
+    {true=> {false=> NationalCall, true=> LocalCall},
+    #If country codes are different, the area code doesnt matter
+     false => {false=> InternationalCall, true=>InternationalCall} } 
+
+
+    origin = Phone.new(call_info[:numero_origen])
+    destination = Phone.new(call_info[:numero_destino])
+    start_time_stamp = call_info[:fechahora_inicio]
+    end_time_stamp = call_info[:fechahora_fin]
+    call = call_classes[origin.same_country(destination)][origin.same_area_code(destination)]
+    call.new(start_time_stamp, end_time_stamp)
+
+  end
+end
---
+++ telco-billing/spec/call_factory_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'byebug'
+require_relative '../app/model/call/call_factory'
+
+require_relative '../app/model/call/local_call'
+
+describe 'CallFactory' do
+  let(:callFactory) { CallFactory.new }
+  it 'Create a local call' do
+    call_info = { 'numero_origen': "54 011 4444 1111",'numero_destino': "54 011 5555 6666",
+'fechahora_inicio': "20190211;14:30", 'fechahora_fin': "20190211;14:31"}
+    local_call = callFactory.new_call(call_info)
+    expect(local_call.class).to eq LocalCall
+  end
+end
