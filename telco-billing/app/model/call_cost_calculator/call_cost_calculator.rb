3eec22d16cc825ab852a9599a079713373584f2e
tomaspinto <tomasp834@gmail.com>
2019-04-09 16:48:38 -0300

    Preparing refactor to remove cost logic from call, add call_cost_calculator and replace minute_counter with minute_cost_calculator

---
+++ telco-billing/app/model/call_cost_calculator/call_cost_calculator.rb
@@ -0,0 +1,7 @@
+class CallCostCalculator
+  def initialize
+    @international_call_cost_calculator = MinuteCostCalculator.new(1)
+    @national_call_cost_calculator = MinuteCostCalculator.new(1)
+    @local_call_cost_calculator = MinuteCostCalculator.new(1)
+  end
+end
