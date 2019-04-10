d66df3da4bb5530e8adb01c198655ac4b7441e27
tomaspinto <tomasp834@gmail.com>
2019-04-10 01:10:21 -0300

    Move some constants to a different file

---
+++ telco-billing/app/model/call_cost_calculator/call_cost_constants.rb
@@ -0,0 +1,15 @@
+$afternoon = 19
+$morning = 8
+$midnight = 0
+$night = 23
+$one_minute_in_seconds = 60
+
+# Time library convention
+
+$sunday = 0
+$monday = 1
+$tuesday = 2
+$wednesday = 3
+$thursday = 4
+$friday = 5
+$saturday = 6
--- telco-billing/app/model/call_cost_calculator/local_call_cost_calculator.rb
+++ telco-billing/app/model/call_cost_calculator/local_call_cost_calculator.rb
@@ -1,5 +1,7 @@
 require_relative '../../aux/functions'
 require 'byebug'
+require_relative 'call_cost_constants'
+
 class LocalCallCostCalculator
   def initialize
     @weekends_cost = 2.1
@@ -8,17 +10,14 @@ class LocalCallCostCalculator
   end
 
   def cost(call)
-    afternoon = 19
-    morning = 8
-    midnight = 0
-    night = 23
-    one_minute_in_seconds = 60
-    weekends = { 0 => { 'since' => midnight, 'until' => night }, 6 => { 'since' => midnight, 'until' => night } }
-    week_days = { 1 => { 'since' => morning, 'until' => afternoon }, 2 => { 'since' => morning,
-                                                                            'until' => afternoon },
-                  3 => { 'since' => morning, 'until' => afternoon }, 4 => { 'since' => morning,
-                                                                            'until' => afternoon },
-                  5 => { 'since' => morning, 'until' => afternoon } }
+    weekends = { $sunday => { 'since' => $midnight, 'until' => $night }, $saturday => { 'since' => $midnight, 'until' => $night } }
+
+    week_days = { $monday => { 'since' => $morning, 'until' => $afternoon }, $tuesday => { 'since' => $morning,
+                                                                                           'until' => $afternoon },
+                  $wednesday => { 'since' => $morning, 'until' => $afternoon }, $thursday => { 'since' => $morning,
+                                                                                               'until' => $afternoon },
+                  $friday => { 'since' => $morning, 'until' => $afternoon } }
+
     start = call.call_start_time
     finish = call.call_end_time
     call_duration = finish - start
@@ -26,9 +25,9 @@ class LocalCallCostCalculator
     call_seconds_during_week_days = count_seconds(start, finish, week_days)
     # following var is always >= 0
     call_seconds_during_week_nights = call_duration - call_seconds_during_weekends - call_seconds_during_week_days
-    week_night_mins = call_seconds_during_week_nights / one_minute_in_seconds
-    week_day_mins = call_seconds_during_week_days / one_minute_in_seconds
-    weekends_mins = call_seconds_during_weekends / one_minute_in_seconds
+    week_night_mins = call_seconds_during_week_nights / $one_minute_in_seconds
+    week_day_mins = call_seconds_during_week_days / $one_minute_in_seconds
+    weekends_mins = call_seconds_during_weekends / $one_minute_in_seconds
     @weekends_cost * weekends_mins + @weekday_night_cost * week_night_mins + @weekday_day_cost * week_day_mins
   end
 end
