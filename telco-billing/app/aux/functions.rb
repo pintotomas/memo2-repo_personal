47337a3167e31a5ed98088b4a1c7ad451790635e
tomaspinto <tomasp834@gmail.com>
2019-04-10 00:04:03 -0300

    Refactor, tests now use LocalCallCostCalculator instead of calculating cost itself

--- telco-billing/app/aux/functions.rb
+++ telco-billing/app/aux/functions.rb
@@ -2,16 +2,16 @@ def max(a, b)
   a > b ? a : b
 end
 
-def count_minutes(time_start, time_end, params, minute_counter)
-  # minute_counter is a MinuteCOunter object
+def count_seconds(time_start, time_end, params)
   # time_start and time_end must be Time objects
-  # Counts the minutes between time_start and time_end that satisfy params
+  # Counts the seconds between time_start and time_end that satisfy params
   # params must be a dictionary with the format {day => {since => a, until => a}}
   # day must be an integer that maps to the week day following the Time gem convention
   # a and b must be the hours that you want to consider from the day 'day'
-  # increments minute_counter when a time meets the requirements in params
-  minute_increment = 60
+  # returns how many seconds satisfy params between time_start and time_end
+  minute_increment_in_seconds = 60
   aux_time = time_start
+  seconds_counter = 0
   while aux_time < time_end
     actual_day = aux_time.wday
     actual_hour = aux_time.hour
@@ -19,9 +19,9 @@ def count_minutes(time_start, time_end, params, minute_counter)
 
       start_hour_limit = params[actual_day]['since']
       end_hour_limit = params[actual_day]['until']
-      minute_counter.inc if actual_hour.between?(start_hour_limit, end_hour_limit)
+      seconds_counter += 60 if actual_hour.between?(start_hour_limit, end_hour_limit)
     end
-    aux_time += minute_increment
-
+    aux_time += minute_increment_in_seconds
   end
+  seconds_counter
 end
