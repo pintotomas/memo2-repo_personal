97c36c05efa9e3370ffece047aab24039fcbc756
tomaspinto <tomasp834@gmail.com>
2019-04-10 19:59:09 -0300

    Remove old file

--- telco-billing/app/model/phone/phone_exceptions.rb
+++ telco-billing/app/model/phone/phone_exceptions.rb
@@ -1,10 +1,19 @@
 class PhoneNumberCannotContainCharactersError < TelcoException
+  def initialize(msg = 'Invalid input for phone number information')
+    super
+  end
 end
 
 class PhonesMustBeThirteenDigitsError < TelcoException
+  def initialize(msg = 'Invalid quantity of numbers for the phone number')
+    super
+  end
 end
 
 class InvalidCountryCodeError < TelcoException
+  def initialize(msg = 'Invalid country code for the phone number')
+    super
+  end
 end
 
 class PhoneOriginNumberEqualsPhoneDestinationNumberError < TelcoException
