2a401b291cc2d56c2324045f17ceb4eca294e0bd
tomaspinto <tomasp834@gmail.com>
2019-04-10 01:36:44 -0300

    Refactor, move some constants of phone to a separated file

--- telco-billing/app/model/phone/phone.rb
+++ telco-billing/app/model/phone/phone.rb
@@ -1,25 +1,23 @@
 require_relative '../exceptions/phone_exceptions'
+require_relative 'phone_constants'
 class Phone
   def initialize(string_number_info)
     # receives a string containing the number information, the two first must be the country code,
     # the following three the area code and last, 8 numbers
 
     # delete spaces
+
     string_number_info = string_number_info.delete(" \t\r\n")
-    valide_input(string_number_info)
-    country_code_start = 0
-    country_code_end = 1
-    country_code = string_number_info[country_code_start..country_code_end]
-    validate_country_coude(country_code)
+    raise PhoneNumberCannotContainCharactersError unless string_number_info.scan(/\D/).empty?
+    raise PhonesMustBeThirteenDigitsError unless string_number_info.length == $phone_number_digits
 
-    area_code_start = 2
-    area_code_end = 4
-    area_code = string_number_info[area_code_start..area_code_end]
-    validate_area_code(area_code)
+    country_code = string_number_info[$country_code_start..$country_code_end]
+    raise InvalidCountryCodeError unless $valid_country_codes.include? country_code
 
-    phone_number_start = 5
-    phone_number_end = 12
-    phone_number = string_number_info[phone_number_start..phone_number_end]
+    area_code = string_number_info[$area_code_start..$area_code_end]
+    raise InvalidAreaCodeError unless $valid_area_codes.include? area_code
+
+    phone_number = string_number_info[$phone_number_start..$phone_number_end]
 
     @country_code = country_code
     @area_code = area_code
@@ -31,24 +29,5 @@ class Phone
 
   def phone_number
     @country_code + @area_code + @phone_number
-  end
-
-  private
-
-  def validate_country_coude(country_code)
-    valid_country_codes = %w[54 52 55 39]
-
-    raise InvalidCountryCodeError unless valid_country_codes.include? country_code
-  end
-
-  def validate_area_code(area_code)
-    valid_area_codes = %w[011 314 324 348]
-    raise InvalidAreaCodeError unless valid_area_codes.include? area_code
-  end
-
-  def valide_input(input)
-    phone_number_digits = 13
-    raise PhoneNumberCannotContainCharactersError unless input.scan(/\D/).empty?
-    raise PhonesMustBeThirteenDigitsError unless input.length == phone_number_digits
   end
 end
---
+++ telco-billing/app/model/phone/phone_constants.rb
@@ -0,0 +1,9 @@
+$country_code_start = 0
+$country_code_end = 1
+$area_code_start = 2
+$area_code_end = 4
+$phone_number_start = 5
+$phone_number_end = 12
+$valid_country_codes = %w[54 52 55 39]
+$valid_area_codes = %w[011 314 324 348]
+$phone_number_digits = 13
