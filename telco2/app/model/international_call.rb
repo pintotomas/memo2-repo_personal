dc0567e77a84a4af3e1a668c9d04107c2e4c6469
tomaspinto <tomasp834@gmail.com>
2019-04-17 12:57:24 -0300

    Test International call of one minute to america during weekend cost

--- telco2/app/model/international_call.rb
+++ telco2/app/model/international_call.rb
@@ -1,6 +1,9 @@
+require_relative '../aux/functions'
+
 class InternationalCall
   DAY_DURATION = 24
   HOUR_DURATION = 60
+  WEEKENDS_DISCOUNT = 0.5
   MIN_COST_PER_COUNTRY_CODE = { '52' => 10, '55' => 6, '39' => 15 }.freeze
 
   attr_reader :start_date_time
@@ -11,9 +14,13 @@ class InternationalCall
     @start_date_time = start_date_time
     @end_date_time = end_date_time
     @call_duration = ((end_date_time - start_date_time) * DAY_DURATION * HOUR_DURATION).to_i
+    weekends_info = { 0 => { 'since' => 0, 'until' => 23 }, 6 => { 'since' => 0, 'until' => 23 } }
+    @weekend_minutes = count_seconds(@start_date_time, @end_date_time, weekends_info) / 60
   end
 
   def cost
-    @call_duration * MIN_COST_PER_COUNTRY_CODE[@country_code]
+    week_cost = (@call_duration - @weekend_minutes) * MIN_COST_PER_COUNTRY_CODE[@country_code]
+    weekends_cost = @weekend_minutes * MIN_COST_PER_COUNTRY_CODE[@country_code] * WEEKENDS_DISCOUNT
+    week_cost + weekends_cost
   end
 end
