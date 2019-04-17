e79eea56aaa6d7e26eb75fa048c47c66d1f7c17d
tomaspinto <tomasp834@gmail.com>
2019-04-17 16:34:53 -0300

    call to rest of world 3 min from weekend to weekday (works on my machine)

--- telco2/spec/international_call_spec.rb
+++ telco2/spec/international_call_spec.rb
@@ -117,5 +117,12 @@ describe 'InternationalCall' do
       call = InternationalCall.new(phone_number_destiny, start_date_time, end_date_time)
       expect(call.cost).to eq 15
     end
+    it 'cost of a call that lasts three minutes should be 30 (from weekend to weekday)' do
+      start_date_time = DateTime.new(2019, 2, 10, 23, 58)
+      end_date_time = DateTime.new(2019, 2, 11, 0, 1)
+
+      call = InternationalCall.new(phone_number_destiny, start_date_time, end_date_time)
+      expect(call.cost).to eq 30
+    end
   end
 end
