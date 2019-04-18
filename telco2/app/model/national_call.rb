96b8af6a79019694da799f0ca2d15f50bc050705
tomaspinto <tomasp834@gmail.com>
2019-04-18 03:47:49 -0300

    add attribute reader to national call, this was causings bugs

--- telco2/app/model/national_call.rb
+++ telco2/app/model/national_call.rb
@@ -6,7 +6,8 @@ class NationalCall
   COST_PER_MINUTE = 1.5
 
   attr_reader :start_date_time
-
+  attr_reader :phone_number_destiny
+  
   def initialize(phone_number_destiny, start_date_time, end_date_time)
     @phone_number_destiny = phone_number_destiny
     @start_date_time = start_date_time
