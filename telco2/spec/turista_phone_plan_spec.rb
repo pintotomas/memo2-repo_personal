73275c15d972b8e14112d0f1c5b18ee5950cedcd
tomaspinto <tomasp834@gmail.com>
2019-04-18 04:34:26 -0300

    turismo test: when making an internationall call to an uncovered country, the price should not be affected

--- telco2/spec/turista_phone_plan_spec.rb
+++ telco2/spec/turista_phone_plan_spec.rb
@@ -2,6 +2,7 @@ require_relative '../app/model/turista_phone_plan'
 require_relative '../app/model/phone_number'
 require_relative '../app/model/local_call'
 require_relative '../app/model/national_call'
+require_relative '../app/model/international_call'
 
 describe 'TuristaPhonePlan' do
   plan = TuristaPhonePlan.new('55')
@@ -30,6 +31,17 @@ describe 'TuristaPhonePlan' do
                                       start_date_time, end_date_time)
     it 'the call price shouldnt be affected' do
       expect(plan.cost(call_to_random)).to eq 20
+    end
+  end
+
+  context 'when making an international call to a country not covered by the plan' do
+    random = '5231403034567'
+    start_date_time = DateTime.new(2019, 3, 11, 14)
+    end_date_time = DateTime.new(2019, 3, 11, 14, 1)
+    call_to_random = InternationalCall.new(PhoneNumber.new(random),
+                                           start_date_time, end_date_time)
+    it 'the call price shouldnt be affected' do
+      expect(plan.cost(call_to_random)).to eq 10
     end
   end
 end
