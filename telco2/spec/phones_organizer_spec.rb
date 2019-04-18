7fedebf850e379dcc08fa0364990715f1fc0dc8e
tomaspinto <tomasp834@gmail.com>
2019-04-18 00:57:19 -0300

    fix rubocop warning

--- telco2/spec/phones_organizer_spec.rb
+++ telco2/spec/phones_organizer_spec.rb
@@ -9,7 +9,7 @@ require_relative '../app/model/errors/only_one_plan_per_phone'
 #  context 'when one plan has been already added' do
 #    it 'adding a new plan raises an exception' do
 #      expect do
-#        phone_organizer.register_plan('5401112345678', 'amigos', 
+#        phone_organizer.register_plan('5401112345678', 'amigos',
 # ['5401134343437', '5401134343438', '5401134343439'])
 #      end.to raise_error(OnlyOnePlanPerPhone)
 #
