07f1db1470058b7c14f374756c67946871d5ae13
tomaspinto <tomasp834@gmail.com>
2019-04-18 14:15:16 -0300

    replace magic numbers

--- telco2/app/model/turista_phone_plan.rb
+++ telco2/app/model/turista_phone_plan.rb
@@ -2,6 +2,9 @@ require_relative '../aux/functions'
 
 class TuristaPhonePlan
   MINUTE_COST = 1.5
+  MINIMUN_COST = 20
+  MIN_COST_UNTIL = 5 # minutes
+  ZERO = 0
   def initialize(country_code)
     @country_code = country_code
     @base_cost = 300
@@ -13,6 +16,6 @@ class TuristaPhonePlan
     return call.cost unless call.phone_number_destiny.country_code == @country_code
 
     duration = call.total_duration
-    20 + max((duration - 5) * MINUTE_COST, 0)
+    MINIMUN_COST + max((duration - MIN_COST_UNTIL) * MINUTE_COST, ZERO)
   end
 end
